b90639b134c8e487e8f204d748208fe9c7bf99dd
Catherine Njenga <freshiah.catherine@gmail.com>
2024-11-11 17:05:13 +0300

    uibuild

--- models/person.js
+++ models/person.js
@@ -16,8 +16,16 @@ mongoose.connect(url)
  })
 
  const personSchema = new mongoose.Schema({
-  name: String,
-  number: Number,
+  name: {
+    type: String,
+    minLength: 5,
+    required: true
+  },
+  number: {
+    type: Number,
+    minLength: 10,
+    required: true
+  },
 })
 
 personSchema.set('toJSON', {
